7ac33e7e03fed92b85f49645101ec830db48f706
Giovanna Sanches <71357714+moghisk@users.noreply.github.com>
2025-04-21 17:09:49 -0300

    Delete TipoToken.java

--- TipoToken.java
+++
@@ -1,8 +0,0 @@
-package ap1_parcial;
-
-public enum TipoToken {
-	PCDec, PCProg, PCInt, PCReal, PCLer, PCImprimir, PCSe, PCEntao, 
-	PCSenao, PCEnqto, PCIni, PCFim, OpAritMult, OpAritDiv, OpAritSoma, OpAritSub,
-	OpRelMenor, OpRelMenorIgual, OpRelMaior, OpRelMaiorIgual, OpRelIgual, OpRelDif,
-	OpBoolE, OpBoolOu, Delim, Atrib, AbrePar, FechaPar, NumInt, NumReal, Cadeia, EOF, Var
-}
